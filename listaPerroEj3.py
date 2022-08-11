9f3047107518e8893393b9e55781770269726da8
Natalia24v <101484974+Natalia24v@users.noreply.github.com>
2022-08-11 20:48:28 -0300

    agrego cambios en lista
    cambiando elemento 2 y 4

--- listaPerroEj3.py
+++ listaPerroEj3.py
@@ -1,1 +1,6 @@
 perro = [13, "Puppy", 12/12/2012, "Macho", 123];
+print("Contenido de la lista original:", perro);
+perro[2] = "13/12/2012";
+print("Nuevo contenido de la lista 1: ", perro);
+perro[4] = 28957346;
+print("Nuevo contenido de la lista 2: ", perro);
